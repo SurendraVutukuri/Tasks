50b57e52e02577e8d9fb41d410a3b9d863b2bce5
Surendra Vutukuri <133015222+SurendraVutukuri@users.noreply.github.com>
2025-07-25 17:18:39 +0530

    Add files via upload

---
+++ service/StudentService.java
@@ -0,0 +1,8 @@
+
+package service;
+import model.Student;
+public class StudentService {
+	public void StudentDetails(Student s) { 
+		System.out.println("Name: " + s.name + ", Age: " + s.age); 
+		} 
+}
